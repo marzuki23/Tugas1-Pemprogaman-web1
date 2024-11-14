8af3c41ee3fb9832499da389e44757781dd28736
marzuki23 <muh.idrismarzuki2301@gmail.com>
2024-11-14 17:44:00 +0700

    update terbaru

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ function toggleMenu() {
 }
 
 document.getElementById("show-more").addEventListener("click", function(event) {
-    event.preventDefault(); // Mencegah halaman bergulir ke atas
+    event.preventDefault();
     const extraContent = document.querySelector(".extra-content");
-    extraContent.classList.toggle("show"); // Menampilkan atau menyembunyikan extra-content
+    extraContent.classList.toggle("show");
 
-    // Ubah teks tombol ketika diklik
     if (extraContent.classList.contains("show")) {
-        this.textContent = "Lebih Sedikit"; // Ubah teks menjadi "Lebih Sedikit" saat konten ditampilkan
+        this.textContent = "Lebih Sedikit";
     } else {
-        this.textContent = "Selengkapnya"; // Ubah teks kembali ke "Selengkapnya" saat konten disembunyikan
+        this.textContent = "Selengkapnya";
     }
 });
